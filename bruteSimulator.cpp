e3540b1496382d335cc291c5aac411f90b51eec9
Pedro Breder Costa <pbrederc@gmail.com>
2025-04-15 18:37:18 -0300

    Menu de opcoes; Evitar senhas vazias; Comentarios

--- bruteSimulator.cpp
+++ bruteSimulator.cpp
@@ -1,52 +1,86 @@
 #include <iostream>
 #include <string>
-#include <chrono>
+#include <chrono> // Biblioteca para medir intervalos de tempo
 
-using namespace std;
+using namespace std; // Uso do namespace para simplificar a escrita, visto que é um codigo para aprendizado
 using namespace chrono;
 
-string SENHA;
-string CHARS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%¨&*()_-+=\\\'\"";
-bool encontrada = false;
-
-void teste(string provisoria, int tamanho, int& tentativas){
+void forcaBruta(string tentativaAtual, int tamanho, string charset, int& tentativas, string senha, bool& encontrada){
 
     if (encontrada){
 
         return;
     }
         
-    if(provisoria.length() == tamanho){
+    if(tentativaAtual.length() == tamanho){
 
-        cout << "Combinação: " << provisoria << endl;
         tentativas++;
         
-        if(provisoria == SENHA){
+        if(tentativaAtual == senha){
 
-            cout << "\nSenha encontrada: " << provisoria << endl;
+            cout << "\nSenha encontrada: " << tentativaAtual << endl;
             cout << "Tentativas: " << tentativas << endl;
             encontrada = true;
         }
     }
     else{
 
-        for(char c : CHARS){
+        for(char caractere : charset){
 
-            teste(provisoria + c, tamanho, tentativas);
+            forcaBruta(tentativaAtual + caractere, tamanho, charset, tentativas, senha, encontrada); // Uso de recursão (Não é a melhor escolha, mas no momento sera essa a escolhida)
         }
     }
 }
 
-int main() {
+int main(){
 
-    int chances = 0;
+    string senha;
+    string chars;
+    int tentativas = 0; // Contador para as tentativas até encontrar a senha
+    int opcao;
+    bool encontrada = false;
 
-    cout << "Digite sua senha:\n";
-    getline(cin, SENHA);
+    do{
 
-    auto inicio = high_resolution_clock::now();
+        cout << "Digite sua senha:\n";
+        getline(cin, senha);
+    }
+    while(senha == "");    // Evitar senhas vazias
 
-    for(int i = 1; i < 101; i++){
+    // Menu 
+    cout << "|---------- Menu Simulador ----------|" << endl;
+    cout << "1. Apenas letras minusculas." << endl;
+    cout << "2. Todas as letras." << endl;
+    cout << "3. Apenas números." << endl;
+    cout << "4. Letras e números." << endl;
+    cout << "5. Letras, numeros e simbolos." << endl;
+    cout << "Escolha uma das opcoes: " << endl;
+    cin >> opcao;
+
+    switch (opcao) {
+        case 1:
+            chars = "abcdefghijklmnopqrstuvwxyz";
+            break;
+        case 2:
+            chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            break;
+        case 3:
+            chars = "0123456789";
+            break;
+        case 4:
+            chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            break;
+        case 5:
+            chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&*()_-+=\\'";
+            break;
+        default: // Qualquer numero que nao esteja no conjunto de numeros definidos
+            cout << "Opção inválida. Encerrando o programa." << endl;
+            return 1;
+    }
+
+    auto inicio = high_resolution_clock::now(); // Uso do chrono para cronometrar
+
+    for(int i = 1; i <= 24; i++){
 
         if (encontrada){
 
@@ -55,14 +89,21 @@ int main() {
         
         else{
             
-            teste("", i, chances);
+            cout << "Testando senha com " << i << " caracteres" << endl;
+            forcaBruta("", i, chars, tentativas, senha, encontrada);
         }
     }
 
-    auto fim = high_resolution_clock::now();
-    auto intervalo = duration_cast<milliseconds>(fim - inicio);
+    if(!encontrada){ // Devido ás condções limitantes do codigo, podem haver casos onde a senha nao é encontrada
+
+        cout << "Senha não foi encontrada. Teste com um charset diferente ou aumente a quantidade de caracteres.";
+    }
+
+    auto fim = high_resolution_clock::now(); // Marca o momento em que foi encontrado a senha
+    auto intervalo = duration_cast<milliseconds>(fim - inicio); // Faz o calculo do intervalo
 
     cout << "Intervalo de tempo: " << intervalo.count() << "ms" << endl;
 
     return 0;
 }
+
